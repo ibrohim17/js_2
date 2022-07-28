15c0c034c2bdb1c8fa703f84736f7584b7e82063
ibrohim17 <ibragimkarimjonov@gmail.com>
2022-07-28 16:59:03 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+alert("Welcome to our disco!")
+
+
+var firstName = prompt("What is your name?", "Ibrohim").trim();
+var lastName = prompt("What is your surname?", "Karimjonov").trim();
+
+
+console.log(`Answers: ${firstName} ${lastName} `);
+
+
+var balls = prompt("How old are you?");
+
+
+var minBalls = 16;
+
+
+if (balls >= minBalls) {
+  alert("You can enter :)");
+} else {
+  alert("You come when you are over 16! :)")
+}
+
+
+alert("Thanks!")
